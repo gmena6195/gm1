d3d800b9c696c468da6ebafc7b7953e78b285ea5
Gmena <gmena@depaul.edu>
2023-02-07 22:05:19 -0600

    first model

--- items/models.py
+++ items/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Items(models.Model):
+    item_name = models.CharField(max_length=30)
+    description = models.CharField(max_length=255)
+
+def __str__(self) -> str:
+    return self.item_name + " " + self.description
