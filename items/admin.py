df7bfbc1c08ecb11569c616a629f88344bb325f8
Gmena <gmena@depaul.edu>
2023-02-07 22:44:18 -0600

    model item

--- items/admin.py
+++ items/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
+from .models import Items
 
 # Register your models here.
-from .models import items
-
-# Register your models here.
-admin.site.register(items)
+admin.site.register(Items
+)
--- items/models.py
+++ items/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Items(models.Model):
-    item_name = models.CharField(max_length=30)
-    description = models.CharField(max_length=255)
+    item_name = models.CharField(max_length=60)
+    description = models.CharField(max_length=60)
 
 def __str__(self) -> str:
     return self.item_name + " " + self.description
